4d00354e507d608786df1f35ac66f6f7a103b214
Nizar <jediyonizar02@gmail.com>
2023-03-16 15:53:41 +0000

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - Print with the function printf in a program
+ *
  * Return: 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 } 
