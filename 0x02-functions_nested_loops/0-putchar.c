12ce717ec1e93f795b419baf6ed1057397b22515
Nizar <jediyonizar02@gmail.com>
2023-03-21 12:31:11 +0000

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
-
 /**
- * main - Entry point
- * Return: Always 0
+ * main - function
+ * _putchar: print the alphabets putchar
+ * Return: always 0
  */
+
 int main(void)
 {
 	_putchar('_');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef main.h
+#define main.h
+
+int _putchar(char c)
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -10,3 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
