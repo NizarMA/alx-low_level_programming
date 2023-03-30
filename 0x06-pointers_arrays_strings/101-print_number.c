093e05090f65227114ced371668c998471ccf5fa
Nizar <Jediyonizar02@gmail.com>
2023-03-30 22:21:44 +0000

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,7 +13,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n1 = -n;
 	}
 
@@ -21,5 +21,4 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	putchar((n1 % 10) + '0');
 }
