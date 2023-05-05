d46ba9b8369eea0813126d24fcb6aaa81da0e293
Nizar <jediyonizar02@gmail.com>
2023-05-05 01:49:30 +0000

    task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,13 +5,13 @@
  * @index: is the index, starting from 0 of the bit you want to set
  * Return: 1 if it worked, or -1 if an error occurred
  */
-int clear_bit(unsigned long int *n, unsigned int index) 
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if
 		(index >= sizeof(unsigned long int) * 8)
 		{
-			return -1;
+			return (-1);
 		}
 	*n &= ~(1UL << index);
-	return 1;
+	return (1);
 }
