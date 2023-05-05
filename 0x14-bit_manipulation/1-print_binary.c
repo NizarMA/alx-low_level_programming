01151e6b39a8a7a9fb95cc848477ddf5af57b504
Nizar <jediyonizar02@gmail.com>
2023-05-05 01:20:12 +0000

    task 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,44 +6,30 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i;
-	int size = sizeof(unsigned long int) * 8;
+	int bin = sizeof(unsigned long int) * 8 - 1;
 	int starting_zero = 1;
-
-	for
-		(i = size - 1;
-		 i >= 0;
-		 i--)
+	while
+		(bin >= 0)
 		{
 			if
-				((n >> i) & 1)
+				((n >> bin) & 1)
 				{
+					putchar('1');
 					starting_zero = 0;
-					break;
 				}
-		}
-	for
-		(;
-		 i >= 0;
-		 i--)
-		{
-			if ((n >> i) & 1)
-			{
-				printf("1");
-			}
 			else
 			{
-				printf("0");
+				if
+					(!starting_zero)
+					{
+						putchar('0');
+					}
 			}
+			bin--;
 		}
-	if (starting_zero)
-	{
-		for
-			(i = 1;
-			 i < size;
-			 i++)
-			{
-				printf("0");
-			}
-	}
+	if
+		(starting_zero)
+		{
+			putchar('0');
+		}
 }
